27b504770d6baf0fa9d86aa329a1331eb3773b40
chh <635062564@qq.com>
2021-05-30 23:57:21 +0800

    商品列表

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,65 @@
+<template>
+  <ul class="tab">
+    <li class="tab-item"
+        :class="[tabIndex === index ? 'active' : '']" 
+        v-for="(item, index) in tabList" 
+        :key="index"
+        @click="onTab(index)">
+      {{item}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    tabList: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      tabIndex: 0
+    }
+  },
+  methods: {
+    /* 选中tab */ 
+    onTab(index) {
+      this.tabIndex = index
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .tab{
+    display: flex;
+    justify-content: space-between;
+    color: #222;
+  }
+
+  .tab-item{
+    position: relative;
+    flex: 1;
+    text-align: center;
+    list-style: none;
+    line-height: 40px;
+  }
+
+  .active{
+    color: var(--color-high-text);
+  }
+
+  .active::after{
+    position: absolute;
+    content: '';
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 2px;
+    width: 40px;
+    margin: 0 auto;
+    background-color: var(--color-high-text);
+    border-radius: 1px;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,15 +4,34 @@
     <home-swiper :banners="banners"/>
     <home-recommend :recommends="recommends" />
     <home-population/>
+    <tab-control :tabList="tabList"/>
+    <ul>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+      <li>111</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import TabControl from 'content/tabControl/TabControl'
+
 import HomeNavBar from './childCpns/HomeNavBar'
 import HomeSwiper from './childCpns/HomeSwiper'
 import HomeRecommend from './childCpns/HomeRecommend'
 import HomePopulation from './childCpns/HomePopulation'
-import { getHomeMultidata } from 'network/home.js'
+
+import { getHomeMultidata, getHomeData } from 'network/home.js'
+
 
 
 
@@ -22,12 +41,16 @@ export default {
     HomeSwiper,
     HomeRecommend,
     HomePopulation,
+    TabControl
   },
-  data() {
-    
+  data() {   
     return {
       banners: [], // 轮播图
       recommends: [], // 推荐
+      tabList: ['流行', '新款', '精选'],
+      good: {
+        'pop': { page: 0, list: [] },
+      }
     }
   },
   /* -----------------------------页面生命周期------------------------ */
